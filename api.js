fa619e6a631941ffd671b5673c11b13a21357e39
HeuvelineThomas <thomasheuveline61@gmail.com>
2021-01-21 15:56:11 +0100

    (refacto) add function delete

--- api.js
+++ api.js
@@ -3,7 +3,7 @@ const bp = require('body-parser');
 
 const { Service } = require('./db/index');
 
-const { getServices, getOneService, postServices, putService } = require('./lib/services.js')
+const { getServices, getOneService, postServices, putService, deleteService } = require('./lib/services.js')
 
 const api = express();
 
@@ -23,30 +23,7 @@ api.get('/services/:id', getOneService);
 api.post('/services', postServices);
 
 api.put('/services/:id', putService);
-api.delete('/services/:id', async(req, res) => {
-    const { id } = req.params;
 
-    let service;
-
-    try {
-        service = await Service.findOne({ _id: id });
-    } catch (err) {
-        res.status(400).json(err);
-    }
-
-    if (service) {
-        try {
-            await service.remove();
-        } catch (err) {
-            res.status(500).json(err);
-        }
-
-        res.status(204).end();
-    } else {
-        res.status(404).json({
-            message: "Service not found !"
-        });
-    }
-});
+api.delete('/services/:id', deleteService);
 
 module.exports = { api };
