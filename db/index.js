ef5c9f22423fdecf24992ceffdffb51e55d75a4a
chuya.fr <chuya.fr@gmail.com>
2021-01-22 15:46:59 +0100

    API works on postman except put

--- db/index.js
+++ db/index.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const { Service } = require('./model/Service')
 
-mongoose.connect('mongodb://localhost/demo', {
+mongoose.connect('mongodb://localhost:27017/services', {
     useNewUrlParser: true,
     useUnifiedTopology: true
 });
